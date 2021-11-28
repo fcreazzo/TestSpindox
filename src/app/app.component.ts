ba2f0ca17df50b5834a1d68b0528118f839005a5
f.creazzo <f.creazzo@hotmail.it>
2021-11-28 22:45:48 +0100

    add service call onInit to get user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-spindox';
+
+  constructor(private userService: UserService) {}
+
+ngOnInit() {
+
+  this.userService.getUser()
+  .subscribe(user => {
+    console.log('USER: ', user);
+  });
+}
+
 }
