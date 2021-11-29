202a594499924a85b4af2422ebf423d29abbb157
f.creazzo <f.creazzo@hotmail.it>
2021-11-30 00:48:46 +0100

    add icons bar and messages to show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,47 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent implements OnInit {
   user: any;
-  userDataToDisplay: string = '';
+  userDataToDisplay: {title: string, data: string} = {title: '', data: ''};
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
 
-ngOnInit() {
+  ngOnInit() {
 
-  this.userService.getUser()
-  .subscribe(user => {
-    console.log('USER: ', user);
-    this.user = user?.results[0];
-  });
-}
+    this.userService.getUser()
+      .subscribe(user => {
+        this.user = user?.results[0];
+      });
+  }
 
-onIconHover(event: string) {
-  this.userDataToDisplay = event;
-}
+  onIconHover(event: string) {
+
+    switch (event) {
+      case ('name'):
+        const name = this.user?.name;
+        this.userDataToDisplay = {title: 'Hi, My name is', data: `${name.first} ${name.last}`};
+        break;
+      case ('email'):
+        this.userDataToDisplay = {title: 'My email adrres is', data: this.user?.email};
+        break;
+      case ('calendar'):
+        const date = new Date(this.user?.dob?.date);
+        this.userDataToDisplay = {title: 'My birthday is', data: date.toLocaleDateString('en-GB')};
+        break;
+      case ('address'):
+        const street = this.user?.location?.street;
+        this.userDataToDisplay = {title: 'My email address is', data: `${street.number} ${street.name}`};
+        break;
+      case ('phone'):
+        this.userDataToDisplay = {title: 'My phone numer is', data: this.user?.phone};
+        break;
+      case ('password'):
+        this.userDataToDisplay = {title: 'My password is', data: this.user?.login?.password};
+        break;
+    }
+  }
+
+  onIconOut() {
+    this.userDataToDisplay = {title: '', data: ''};
+  }
 
 }
